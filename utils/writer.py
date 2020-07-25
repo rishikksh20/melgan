9cbe1f5293887f30913a598c02eefa5243f8a0f4
rishikksh20 <rishikksh20@gmail.com>
2020-07-25 14:14:35 +0530

    Add melSpectrogram plotting on tensorboard

--- utils/writer.py
+++ utils/writer.py
@@ -1,12 +1,19 @@
 from tensorboardX import SummaryWriter
-
-from .plotting import plot_waveform_to_numpy
-
+from utils.stft import TacotronSTFT
+from .plotting import plot_waveform_to_numpy, plot_spectrogram_to_numpy
+import torch
 
 class MyWriter(SummaryWriter):
     def __init__(self, hp, logdir):
         super(MyWriter, self).__init__(logdir)
         self.sample_rate = hp.audio.sampling_rate
+        self.stft = TacotronSTFT(filter_length=hp.audio.filter_length,
+                            hop_length=hp.audio.hop_length,
+                            win_length=hp.audio.win_length,
+                            n_mel_channels=hp.audio.n_mel_channels,
+                            sampling_rate=hp.audio.sampling_rate,
+                            mel_fmin=hp.audio.mel_fmin,
+                            mel_fmax=hp.audio.mel_fmax)
         self.is_first = True
 
     def log_training(self, g_loss, d_loss, adv_loss, step):
@@ -20,13 +27,20 @@ class MyWriter(SummaryWriter):
         self.add_scalar('validation.adv_loss', adv_loss, step)
         self.add_audio('raw_audio_predicted', prediction, step, self.sample_rate)
         self.add_image('waveform_predicted', plot_waveform_to_numpy(prediction), step)
-
+        wav = torch.from_numpy(prediction).unsqueeze(0)
+        mel = self.stft.mel_spectrogram(wav)  # mel [1, num_mel, T]
+        self.add_image('melspectrogram_prediction', plot_spectrogram_to_numpy(mel.squeeze(0).data.cpu().numpy()),
+                       step, dataformats='HWC')
         self.log_histogram(generator, step)
         self.log_histogram(discriminator, step)
 
         if self.is_first:
             self.add_audio('raw_audio_target', target, step, self.sample_rate)
             self.add_image('waveform_target', plot_waveform_to_numpy(target), step)
+            wav = torch.from_numpy(target).unsqueeze(0)
+            mel = self.stft.mel_spectrogram(wav)  # mel [1, num_mel, T]
+            self.add_image('melspectrogram_target', plot_spectrogram_to_numpy(mel.squeeze(0).data.cpu().numpy()),
+                           step, dataformats='HWC')
             self.is_first = False
 
     def log_histogram(self, model, step):
